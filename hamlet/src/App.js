fea43f176fb7f7e647f32a778e3260d15a36cbeb
jasonhafner <jasonhowardhafner@gmail.com>
2025-07-21 08:08:47 -0500

    July21

--- hamlet/src/App.js
+++ hamlet/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          H.A.M.L.E.T. App
+          H.A.M.L.E.T. App Mondays :|
         </p>
       </header>
     </div>
